230a0e187fd9e222383202df067e51767ae829b1
Thomas Thomassen <thomas@thomthom.net>
2013-07-21 15:56:11 +0200

    Implemented SketchupExtension support and restructured project folder.

--- src/tt_selection_memory/core.rb
+++ src/tt_selection_memory/core.rb
@@ -17,57 +17,33 @@ require 'sketchup.rb'
 #-------------------------------------------------------------------------------
 
 
-# <hotfix>
-# Need to ensure the parent namespaces exists. Normally TT_Lib2 defines this.
-module TT;end
-module TT::Plugins;end
-# </hotfix>
 module TT::Plugins::SelectionMemory
-  
-  
+
+
   ### CONSTANTS ### ------------------------------------------------------------
-  
-  # Plugin information
-  PLUGIN_ID       = 'TT_SelectionMemory'.freeze
-  PLUGIN_NAME     = 'Selection Memory'.freeze
-  PLUGIN_VERSION  = '1.0.1'.freeze
-  
+
   # Modify this constant to adjust the selection size to suit your needs.
   SELECTION_STACK_SIZE = 5
-  
+
   ### VARIABLES ### ------------------------------------------------------------
-  
+
   @selection = Array.new( SELECTION_STACK_SIZE, [] )
   @index = 0
   @restoring = false
-  
-  
+
+
   ### MENU & TOOLBARS ### ------------------------------------------------------
-  
+
   unless file_loaded?( __FILE__ )
     # Menus
     menu = UI.menu( 'Edit' )
     menu.add_item( 'Cycle Previous Selections' ) { self.cycle_selections }
-  end 
-  
-  
-  ### LIB FREDO UPDATER ### ----------------------------------------------------
-  
-  def self.register_plugin_for_LibFredo6
-    {   
-      :name => PLUGIN_NAME,
-      :author => 'thomthom',
-      :version => PLUGIN_VERSION.to_s,
-      :date => '09 Jan 12',
-      :description => 'Cycles through previous selections.',
-      :link_info => 'http://forums.sketchucation.com/viewtopic.php?f=323&t=42469'
-    }
   end
-  
-  
+
+
   ### MAIN SCRIPT ### ----------------------------------------------------------
 
-  
+
   # @since 1.0.0
   def self.cycle_selections
     @index = ( @index + 1 ) % @selection.size
@@ -77,8 +53,8 @@ module TT::Plugins::SelectionMemory
     Sketchup.active_model.selection.add( valid_selection )
     @restoring = false
   end
-  
-  
+
+
   # @since 1.0.0
   def self.cache_selection( selection )
     unless @restoring
@@ -87,62 +63,63 @@ module TT::Plugins::SelectionMemory
       @index = 0
     end
   end
-  
+
+
   # @since 1.0.0
   def self.clear_selection( selection )
     unless @restoring
       @index = SELECTION_STACK_SIZE - 1
     end
   end
-  
-  
+
+
   # @since 1.0.0
   def self.reset
     @selection = Array.new( SELECTION_STACK_SIZE, [] )
     @index = 0
     @restoring = false
   end
-  
-  
+
+
   # @since 1.0.0
   class SelectionMemoryObserver < Sketchup::SelectionObserver
-    
+
     # @since 1.0.0
     def onSelectionBulkChange( selection )
       TT::Plugins::SelectionMemory.cache_selection( selection )
     end
-    
+
     def onSelectionCleared( selection )
       TT::Plugins::SelectionMemory.clear_selection( selection )
     end
-    
+
   end
-  
-  
+
+
   # @since 1.0.0
   class SelectionMemoryAppObserver < Sketchup::AppObserver
-  
+
     # @since 1.0.0
     def onNewModel( model )
       observe_selection( model )
     end
-    
+
     # @since 1.0.0
     def onOpenModel( model )
       observe_selection( model )
     end
-    
+
     private
-    
+
     # @since 1.0.0
     def observe_selection( model )
       model.selection.add_observer( SelectionMemoryObserver.new )
       TT::Plugins::SelectionMemory.reset
     end
-    
+
   end
-  
-  
+
+
   # @since 1.0.0
   unless file_loaded?( __FILE__ )
     Sketchup.add_observer( SelectionMemoryAppObserver.new )
@@ -150,19 +127,32 @@ module TT::Plugins::SelectionMemory
   end
 
 
-  
   ### DEBUG ### ----------------------------------------------------------------
-  
+
   # @note Debug method to reload the plugin.
   #
   # @example
-  #   TT::Plugins::SelectionMemory.reload
+  #   TT::Plugins::Template.reload
   #
+  # @param [Boolean] tt_lib Reloads TT_Lib2 if +true+.
+  #
+  # @return [Integer] Number of files reloaded.
   # @since 1.0.0
   def self.reload( tt_lib = false )
     original_verbose = $VERBOSE
     $VERBOSE = nil
+    TT::Lib.reload if tt_lib
+    # Core file (this)
     load __FILE__
+    # Supporting files
+    if defined?( PATH ) && File.exist?( PATH )
+      x = Dir.glob( File.join(PATH, '*.{rb,rbs}') ).each { |file|
+        load file
+      }
+      x.length + 1
+    else
+      1
+    end
   ensure
     $VERBOSE = original_verbose
   end
